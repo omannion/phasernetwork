53499ea88f823b669295c752f5abe55869cfd8e3
Oliver Mannion <oligolli@yahoo.com>
2016-08-22 21:48:00 +0100

    Uploaded previous work

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/public'));
+app.get('/', function(req, res,next){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+  socket.on('leftStatus', function(msg){
+    console.log('Left button: ' + msg);
+  });
+
+
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
